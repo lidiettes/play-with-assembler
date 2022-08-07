3f8be40266b82815d36bb1c38970bb7a1b918681
lidiettes <hello@lidiettes.com>
2022-08-07 22:57:42 +0200

    change time and solve errors

--- script.js
+++ script.js
@@ -10,73 +10,73 @@ let userName = document.querySelector("#user-name");
 let startGame = document.querySelector("#start-game");
 let clickHere = document.querySelector("#click-here");
 let playAgain = document.querySelector("#play-again");
-let scorePlayer = document.querySelector(".score");
+let scorePlayer = document.querySelector("#score");
 let timeRandom = Math.floor(Math.random() * 11000); // get ready button random time
 let myTimeout = 0;
 
+var timer = 0;
+var startTime;
+var finishTime;
+
 // OBJECT
 let myPlayers = {
-  totalPlayers: 0,
   name: [],
   score: [],
 };
-
-//let agregar = [];
+//---------------------------------------//
 
 if (localStorage.getItem("myPlayers") !== null) {
   myPlayers = JSON.parse(localStorage.getItem("myPlayers")); // GET DATES
- // agregar = myPlayers;
 } else {
   scorePlayer.innerHTML = "Sin datos";
 }
 
+//---------------------------------------//
+
 btnStart.addEventListener("click", function startBtn() {
-  myPlayers.totalPlayers++;
+  
   myPlayers.name.push(userName.value);
   
   screen1.classList.add("no-display");
   screen2.classList.remove("no-display");
-  //scorePlayer.innerHTML = JSON.stringify(myPlayers);
-
-  console.log(myPlayers);
-
+  scorePlayer.innerHTML = JSON.stringify(myPlayers); //ESTO NO ENTIENDO SI TODAVIA NO DA PUNTOS
   localStorage.setItem("myPlayers", JSON.stringify(myPlayers)); // SAVE DATES
-  // 
-
+  
+ // userName.innerHTML = JSON.stringify(myPlayers.name); //mi cosecha 2
+ //document.getElementById("username").innerHTML= myPlayers.name;// de mi cosecha
+  //
 });
 
-// CHANGE SCREEN
-
 startGame.addEventListener("click", function startBtn() {
+  timer = 0;
   screen2.classList.add("no-display");
   screen3.classList.remove("no-display");
   myTimeout = setTimeout(letPlay, timeRandom);
 });
 
 function letPlay() {
-  const date1 = new Date().getTime;
+  
   screen3.classList.add("no-display");
   screen4.classList.remove("no-display");
-  clickHere.addEventListener("click", function () {
-    const date2 = new Date().getTime;
-    const Time = date2 - date1; //score
-    scorePlayer.innerHTML = Time / 1000;
-    myPlayers.score.push(Time / 1000);
+  startTime = new Date ();
+
+  clickHere.addEventListener("click", function startBtn() {
+    finishTime = new Date();
+    timer = (finishTime - startTime) / 1000;
+    myPlayers.score.push() = timer;
+    document.getElementById("score").innerHTML = myPlayers.score + " seconds";
+
+   // clearInterval(); // var positionRandomInterval;
+
     localStorage.setItem("myPlayers", JSON.stringify(myPlayers));
     screen4.classList.add("no-display");
     screen5.classList.remove("no-display");
 
     playAgain.addEventListener("click", function () {
+      //que yo sepa aqui no le estmaos diciendo ninguna funcion o si
       screen5.classList.add("no-display");
       screen1.classList.remove("no-display");
     });
   });
+}
 
-  //diference in milliseconds
-}
-function playStart() {
-  return "hoy si";
-}
-function playStart(){
-    return "hoy si";
-} 
